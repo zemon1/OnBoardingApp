99787064bce2f220b7e1772ad377465ef159b566
unknown <jeff.haak@EN-JEFFHAAK-L01.EnerNOC.local>
2014-06-09 09:12:58 -0400

    Functional, could use some improvements

---
+++ onBoarding.py
@@ -0,0 +1,49 @@
+#Jeff Haak
+#OnBoarding Assistant
+import shutil, os, sys, subprocess, winshell
+
+#Unzip files
+#def unzip(fileName):
+
+#Copy Files
+def copyFolder(path):
+    shutil.copytree(path, r'C:\Program Files')
+
+
+if __name__ == "__main__":
+    #Install JDK
+    #os.startfile(r'RequiredFiles\jdk-7u60-windows-x64.exe')
+
+    #Move Maven to Program Files
+    #shutil.copytree(r'RequiredFiles\Maven', r'C:\Program Files\Maven')
+    #(Add maven to windows Path)
+
+    #Move Eclipse to Program Files
+    #shutil.copytree(r'RequiredFiles\eclipse', r'C:\Program Files\eclipse')
+    #Move Eclipse's short cut to the desktop
+    #shutil.copy(r'C:\Program Files\eclipse\Eclipse.lnk', winshell.desktop() + "\Eclipse.lnk")
+
+    #Move SQL Developer to Program Files
+    #shutil.copytree(r'RequiredFiles\sqldeveloper', r'C:\Program Files\sqldeveloper')
+    #Move SQL Developer's short cut to the desktop
+    #shutil.copy(r'C:\Program Files\sqldeveloper\SQL Developer.lnk', winshell.desktop() + "\SQL Developer.lnk")
+
+    #Move Hosts file
+    #shutil.copy(r'RequiredFiles\hosts', r'C:\Windows\System32\drivers\etc\hosts')
+
+    #Install Notepad++
+    #os.startfile(r'RequiredFiles\npp.6.6.3.Installer.exe')
+
+    #Install WinRar
+    #os.startfile(r'RequiredFiles\winrar-x64-501.exe')
+
+    #Install GitBash
+    #os.startfile(r'RequiredFiles\Git-1.9.2-preview20140411.exe')
+
+    #Install TortoiseSVN
+    #os.startfile(r'RequiredFiles\TortoiseSVN-1.8.7.25475-x64-svn-1.8.9.msi')
+
+    #Install UltraVNC
+    #os.startfile(r'RequiredFiles\UltraVNC_1_1_9_X64_Setup.exe')
+
+    
